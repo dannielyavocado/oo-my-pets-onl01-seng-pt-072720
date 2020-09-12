0dbc417702cf695aed01c78f2e3e3eaa7b900fd7
Danniely Santos <dannielyingrid@gmail.com>
2020-09-12 17:35:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    "I have #{dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
   def say_species
